f4df2d58e1da1aef921111577fd5ac93df1699af
“salamzakeer” <salamzakeer123@gmail.com>
2022-02-26 23:00:39 +0530

    frist commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className = "header">
+        <div className="logo">
+          <img src="White logo.png"></img>
+          <h2>Bookyourchef.lk</h2>
+        </div>
+        <ul className="navbar">
+          <li>home</li>
+          <li>home</li>
+          <li>home</li>
+          <li>home</li>
+          <li>home</li>
+        </ul>
+        <button className="button">Sign up</button>
+      </div>
+      <div className = "sectionOne">
+        
+        <img src="ingree.png" className='ingree'></img>
+        <img src="pan.png" className='pan'></img> 
+      </div>
     </div>
   );
 }
